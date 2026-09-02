79a225a871f7b5cff3c92b53121e7cf92e2fb78f
YOGESH H <yogihanumanthappa1212p@gmail.com>
2026-09-02 20:39:23 +0530

    Improve_it

---
+++ palindrome.c
@@ -0,0 +1,21 @@
+bool isPalindrome(int x) {
+    int a;
+    a=x;
+    double r=0;
+    int n;
+    if(x<0){
+        return false;
+    }
+    while(a!=0){
+        r=r*10;
+        n=a%10;
+        a=a/10;
+        r+=n;
+    }
+    if(x==r){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
